a23a12f00405fb09f8715fa2ad6890897cf431a2
Rodrigoifpb <rodrigoflafla@gmail.com>
2021-01-13 21:49:56 -0300

    scriptde setar logs para ERROR

---
+++ SetLog.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#1.0 Rodrigo Pontes ; 26/11/2020
+
+#updating database
+updatedb
+
+locate logback.xml > list.txt
+
+while read base;do
+
+        sed -i 's/DEBUG/ERROR/g' $base &>> setLogsError.logs
+        echo $base
+
+done < list.txt
+
+rm -rf list.txt
+
+exit 0
